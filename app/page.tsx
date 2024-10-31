4eb9b820cf056aad51ac2cc2b6c78a7c70d63135
wasi <hammadwasi.b@gmail.com>
2024-10-31 16:54:23 +0500

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="h-screen w-screen flex items-center justify-center flex-col gap-3">
       <h1 className="text-4xl text-white">Coming Soon</h1>
       <p className="text-white">
-        We're working hard to bring you something amazing. Stay tuned!
+        We&apos;re working hard to bring you something amazing. Stay tuned!
       </p>
     </div>
   );
