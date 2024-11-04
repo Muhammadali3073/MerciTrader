075e6bd5855460a80db8e8530af45ef81eb88fa7
Muhammad Ali Nawaz <78873323+Muhammadali3073@users.noreply.github.com>
2024-11-04 15:43:07 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center flex-col gap-3 bg-[url('/images/hero.jpg')]">
-      <h1 className="text-4xl text-black text-center">Coming Soon</h1>
-      <p className="text-black text-center">
+    <div
+      className="h-screen w-screen flex items-center justify-center flex-col gap-3"
+      style={{ backgroundColor: '#db3b34' }}
+    >
+      <h1 className="text-4xl text-white text-center">Coming Soon</h1>
+      <p className="text-white text-center">
         We&apos;re working hard to bring you something amazing. Stay tuned!
       </p>
     </div>
