de1b2a82ce8a9905fc96e3c877791fa4515eac2e
Muhammad Ali Nawaz <78873323+Muhammadali3073@users.noreply.github.com>
2025-08-13 01:02:14 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
       className="h-screen w-screen flex items-center justify-center flex-col gap-3 bg-white"
     >
       <h1 className="text-4xl text-black text-center">Coming Soon</h1>
-      <p className="text-center text-[#db3b34]">
-        We&apos;re working hard to bring you something amazing. Stay tuned!
-      </p>
     </div>
   );
 }
